c308925275cc2995254633c752233f7f38c00f4b
marianavilarim <n01603357@humber.ca>
2023-11-09 08:59:47 -0500

    clarifies that the code is fetching to hidden

--- script.js
+++ script.js
@@ -5,3 +5,4 @@ const age = document.getElementById("age");
 const myElement = document.getElementById("texth1");
 const myElement2 = document.getElementById("textpplan");
 let myElement3 = document.getElementById("requirements");
+let hidden = myElement3.getAttribute("hidden");
