b999b597b3aae62a08a166a9b4f406c4e3b4750d
marianavilarim <n01603357@humber.ca>
2023-11-09 08:57:26 -0500

    Retrieve the HTML element with the id "textpplan"

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ const YearToday = document.getElementById("yeartoday");
 const age = document.getElementById("age");
 
 const myElement = document.getElementById("texth1");
+const myElement2 = document.getElementById("textpplan");
