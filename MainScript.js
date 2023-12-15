e07dc86667382a4b914dacdb87119adaebe0f8d0
joaopedroalencar <joaopedroalencar@hotmail.com>
2023-12-15 12:19:24 -0500

    function result updated to Jquery

--- MainScript.js
+++ MainScript.js
@@ -18,17 +18,15 @@ function Aboutit(a) {
   $('#textpresult').attr("hidden", "hidden");
 }
 
-
-function Result(a){
-
-    myElement11.innerHTML = "Summary";
-    myElement13.innerHTML = "";
-    myElement14.innerHTML = "";
-    myElement2.setAttribute("hidden", "hidden");
-    myElement3.setAttribute("hidden", "hidden");
-    myElement10.setAttribute("hidden", "hidden");
-    myElement15.removeAttribute("hidden");
- 
+// result updated to Jquery
+function Result(a) {
+  $('#texth1').html("Summary");
+  $('#textp2plan').attr("hidden", "hidden");
+  $('#textp3plan').attr("hidden", "hidden");
+  $('#textpplan').attr("hidden", "hidden");
+  $('#requirements').attr("hidden", "hidden");
+  $('#infotable').attr("hidden", "hidden");
+  $('#textpresult').removeAttr("hidden");
 }
 
 function ClientInfo(b){
